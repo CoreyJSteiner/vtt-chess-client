29613c1bdba28ddf76c93a1f79207db31848fdc7
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-05-04 19:33:48 -0400

    adds generic board and centers

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -6,35 +6,57 @@ import {
 import {
   Container,
   Graphics,
+  Sprite,
+  Assets
 } from 'pixi.js'
-import { useCallback, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 extend({
   Container,
   Graphics,
+  Sprite
 })
 
 const Board: React.FC = () => {
   const [containerElement, setContainerElement] = useState<HTMLDivElement | null>(null)
+  const [texture, setTexture] = useState(null);
 
   //Test Graphic
-  const drawCallback = useCallback((graphics: Graphics) => {
-    graphics.clear()
-    graphics.setFillStyle({ color: 'red' })
-    graphics.rect(0, 0, 50, 50)
-    graphics.fill()
+  // const drawCallback = useCallback((graphics: Graphics) => {
+  //   graphics.clear()
+  //   graphics.setFillStyle({ color: 'red' })
+  //   graphics.rect(0, 0, 50, 50)
+  //   graphics.fill()
+  // }, [])
+
+  useEffect(() => {
+    const loadTexture = async () => {
+      const tex = await Assets.load('src/assets/board-generic.png');
+      setTexture(tex);
+    };
+
+    loadTexture();
   }, [])
 
   return (
     <div id='game-board' ref={setContainerElement}>
       {containerElement && <Application
         resizeTo={containerElement}
+        background={'#fff'}
         autoDensity={true}
         resolution={window.devicePixelRatio || 1}
       >
-        <pixiContainer x={100} y={100}>
+        {texture && (
+          <pixiSprite
+            texture={texture}
+            x={containerElement.offsetWidth / 2}
+            y={containerElement.offsetHeight / 2}
+            anchor={0.5}
+          />
+        )}
+        {/* <pixiContainer x={100} y={100}>
           <pixiGraphics draw={drawCallback} />
-        </pixiContainer>
+        </pixiContainer> */}
       </Application>}
     </div>
   )
