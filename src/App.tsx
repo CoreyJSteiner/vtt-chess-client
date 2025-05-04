651d8d155044a48aed2756fa914f2d6fff187cf4
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-05-04 13:22:54 -0400

    socket io chat connection working (dev only)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import './App.css'
 import Board from './components/Board'
 import Chat from './components/Chat'
+import { socket } from './socket'
+import { useState, useEffect } from 'react'
 
 const App: React.FC = () => {
+  const [messages, setMessages] = useState<Array<string>>([]);
+
+  const onMessageIn = (message: string) => {
+    console.log(message)
+    setMessages(prev => [...prev, message]);
+  }
+
+  const submitHandler = (input: string) => {
+    if (input) {
+        socket.emit('chat-clientOrigin', input)
+    }
+  }
+
+
+  useEffect(() => {
+    //Socket Listeners
+    socket.emit('username set', 'Corey')
+    socket.emit('room set', '1')
+    socket.on('chat-serverOrigin', onMessageIn)
+
+    return () => {
+      socket.off('chat-serverOrigin', onMessageIn)
+    };
+  }, []);
+
   return (
     <>
       <Board />
-      <Chat />
+      <Chat messages={messages} submitMsgHandler={submitHandler}/>
     </>
   )
 }
--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,13 +1,31 @@
 import './Chat.css'
+import { useState } from 'react'
 
-const Chat: React.FC = () => {
+type ChatProps = {
+    messages: Array<string>,
+    submitMsgHandler: (input: string) => void
+}
+
+const Chat: React.FC<ChatProps> = ({messages, submitMsgHandler}) => {
+    const [input, setInput] = useState<string>('')
+
+    const handleClick = () => {
+        submitMsgHandler(input)
+    }
+
     return(
         <div id='chat'>
             <ul>
-                <li>Hey</li>
-                <li>Sup</li>
-                <li>Nm u?</li>
+                {
+                messages.map((message: string, index: number) =>
+                    <li key={ index }>{ message }</li>
+                )
+                }
             </ul>
+            <div id='chat-input'>
+                <input type='text' value={input} onChange={(e) => setInput(e.target.value)}/>
+                <button onClick={handleClick}>Send</button>
+            </div>
         </div>
     )
 }
