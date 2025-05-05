1a27e279089c56dcd114859714364d5f943277df
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-05-05 10:38:14 -0400

    centers board and fixes chat overflow

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,7 +2,7 @@ import './Board.css'
 import {
   Application,
   extend,
-  useTick
+  // useTick
 } from '@pixi/react'
 import {
   Container,
@@ -20,16 +20,35 @@ extend({
 
 const Board: React.FC = () => {
   const [containerElement, setContainerElement] = useState<HTMLDivElement | null>(null)
-  const [texture, setTexture] = useState(null);
+  const [texture, setTexture] = useState(null)
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: 0,
+    height: 0,
+  })
 
   useEffect(() => {
     const loadTexture = async () => {
       const tex = await Assets.load('src/assets/board-generic.png');
       setTexture(tex);
+
+      const handleResize = () => {
+        setWindowDimensions({
+          width: containerElement.offsetWidth || 0,
+          height: containerElement.offsetHeight || 0,
+        })
+      }
+
+      if (containerElement) {
+        window.addEventListener('resize', handleResize)
+      }
+
+      handleResize()
+
+      return () => window.removeEventListener('resize', handleResize)
     };
 
-    loadTexture();
-  }, [])
+    loadTexture()
+  }, [containerElement])
 
   return (
     <div id='game-board' ref={setContainerElement}>
@@ -42,8 +61,8 @@ const Board: React.FC = () => {
         {texture && (
           <pixiSprite
             texture={texture}
-            x={containerElement.offsetWidth / 2}
-            y={containerElement.offsetHeight / 2}
+            x={windowDimensions.width / 2}
+            y={windowDimensions.height / 2}
             anchor={0.5}
           />
         )}
